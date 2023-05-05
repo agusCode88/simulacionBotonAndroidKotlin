312520a3393688ead698c6553596cb0cea619b88
agusCode88 <agus.romero.salazar@gmail.com>
2023-05-05 17:23:04 -0400

    Actividad Principal
    Se sube la tarea de POO

--- app/src/main/java/com/example/acividadboton/MainActivity.kt
+++ app/src/main/java/com/example/acividadboton/MainActivity.kt
@@ -16,62 +16,20 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val botonSimulado = DisenioBoton("Mi botón simulado", "centrado", 20)
-
-        botonSimulado.inflar()
-        botonSimulado.agregarBordes()
-
-        val boton = findViewById<Button>(R.id.boton)
-        boton.setOnClickListener {
-            botonSimulado.onclick()
-        }
+//        val botonSimulado = DisenioBoton("Mi botón simulado", "centrado", 20)
+//
+//        botonSimulado.inflar()
+//        botonSimulado.agregarBordes()
+//
+//        val boton = findViewById<Button>(R.id.boton)
+//        boton.setOnClickListener {
+//            botonSimulado.onclick()
+//        }
 
     }
 }
 
 
-abstract class View(): EventosListener {
-    abstract fun inflar()
-}
 
-open class Button(
-    open var texto: String,
-    open var posicion: String
-): View(), EventosListener, EventosListener2 {
-    override fun inflar() {
-        println(
-            "Hola soy el botón y me estoy pintando, mi texto es: $texto y mi posición en la vista es: $posicion"
-        )
-    }
 
-    override fun cambiarFondo() {
-        println("Se ha cambiado el fondo del botón $texto")
-    }
-
-    override fun onclick() {
-        println("Se ha hecho clic en el botón $texto")
-        super.onclick()
-        cambiarFondo()
-    }
-
-    override fun onTouch() {
-        super<EventosListener>.onTouch()
-        super<EventosListener2>.onTouch()
-    }
-}
-
-class DisenioBoton(
-    override var texto: String,
-    override var posicion: String,
-    var valoresQuinas: Int
-): Button(texto, posicion) {
-    override fun inflar() {
-        super.inflar()
-        println("Le estoy dando bordes al botón $texto con la posición $posicion y con este tamaño de esquinas $valoresQuinas")
-    }
-
-    fun agregarBordes() {
-        println("Se han agregado bordes al botón $texto")
-    }
-}
 
